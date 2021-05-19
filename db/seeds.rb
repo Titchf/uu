8c4241d045b2f62a8c07ab1147d339b7f5eab977
Titch F <github@fauconnier.eu>
2021-05-19 23:48:58 +0200

    ddd

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "so good", address: "Mama's house 334", phone_number: "338338983", category: "chinese")
+
+Restaurant.create(name: "Todo al Pastor", address: "pacific avenue", phone_number: "3535535", category: "french")
+
+Restaurant.create(name: "Chez Diego", address: "yooyoo this is an address", phone_number: "9773922937", category: "belgian")
+
+Restaurant.create(name: "PixZas", address: "Street blaaa", phone_number: "293392428", category: "italian")
+
+Restaurant.create(name: "Molotov Party", address: "Square This", phone_number: "99847367", category: "italian")
